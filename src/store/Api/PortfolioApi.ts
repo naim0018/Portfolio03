1375b969253d604f2a94e0da37c90a64433b317c
Md Kazi Naim <kazinaim@softvence.com>
2026-03-21 15:24:39 +0600

    update api

--- src/store/Api/PortfolioApi.ts
+++ src/store/Api/PortfolioApi.ts
@@ -3,7 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const portfolioApi = createApi({
   reducerPath: "portfolioApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.VITE_API_BASE_URL || "http://localhost:5000/api/v1",
+    baseUrl:
+      import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api/v1",
   }),
   tagTypes: ["Portfolio"],
   endpoints: (builder) => ({
